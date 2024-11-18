c9aa597efc3067370f5921ff03e0eed997a18905
Stefano Scalfari <vorshim92@gmail.com>
2024-11-18 16:57:24 +0100

    testing 3DF

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-import Dashboard from 'dashboard';
-import Alert from 'components/alert';
-
+import "./App.css";
+import Dashboard from "dashboard";
+import Alert from "components/alert";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, PresentationControls } from "@react-three/drei";
 
 function App() {
   return (
     <div className="app app-wrapper">
       <Dashboard className="app app-body" />
       <Alert />
+      <div className="canvas-drone">
+        <Canvas>
+          <PresentationControls enabled={true} global={false} cursor={true} snap={true} speed={1} zoom={1} rotation={[0, 0, 0]} polar={[0, Math.PI / 2]} azimuth={[-Infinity, Infinity]} config={{ mass: 1, tension: 170, friction: 26 }}>
+            <mesh>
+              <boxGeometry />
+              <meshNormalMaterial />
+            </mesh>
+          </PresentationControls>
+        </Canvas>
+      </div>
     </div>
   );
 }
